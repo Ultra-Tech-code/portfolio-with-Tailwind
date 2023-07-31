49124f467a2e8c9c35607cd85ab97e85902f3608
Ultra-tech-code <hizick27@gmail.com>
2023-07-31 04:13:22 +0100

    form and movie section updated

--- process.php
+++ process.php
@@ -2,12 +2,12 @@
   if (isset($_POST['email']))  {
   
     //Email information
-    $admin_email = "brad@edgeledger.net";
+    $admin_email = "meekhizick@gmail.com";
     $email = $_POST['email'];
     $message = $_POST['message'];
     
     //send email
     mail($admin_email, "New Form Submission", $message . ' - ', "From:" . $email);
     
-    header('Location: http://edgeledger.net/success.html');
+    // header('Location: http://edgeledger.net/success.html');
   }
